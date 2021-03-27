bbc0bb8593bc799e4198a4c569cdfda2b78566f3
Neil Panjwani <paniwani@gmail.com>
2021-03-27 15:11:41 -0700

    added random number and numpy

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, SAST!'
+    return 'Hello, SAST' + str(np.random.rand(1)) + '!'
